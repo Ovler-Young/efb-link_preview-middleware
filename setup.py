5a7a96b76e31cfee29e6be51ca85e7aed27f657d
Ovler <chengyujin86@gmail.com>
2024-03-02 22:44:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     install_requires=[
         "ehforwarderbot>=2.0.0b15",
-        "python-telegram-bot>=10.0.0<12.0.0",
+        "python-telegram-bot>=10.0.0<21.0.0",
         "beautifulsoup4",
     ],
     entry_points={
